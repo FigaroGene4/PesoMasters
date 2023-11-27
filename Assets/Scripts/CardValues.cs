4bef39aaa682e4ea3c66d20ff22547f124a0c079
Jedcleofe <129458179+Jedcleofe@users.noreply.github.com>
2023-11-27 14:20:58 +0800

    card value/new cards

---
+++ Assets/Scripts/CardValues.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardValues : MonoBehaviour
+{
+    public int starChange;
+    public int coinsChange;
+    public int energyChange;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/drawCards.cs
+++ Assets/Scripts/drawCards.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class DrawCards : MonoBehaviour
 {
     public GameObject CardExp1, CardExp2, CardExp3, CardExp4, CardExp5, CardExp6, CardExp7, CardExp8, CardExp9, cardExp10;
     public GameObject CardInc1, CardInc2, CardInc3, CardInc4, CardInc5, CardInc6, CardInc7, CardInc8, CardInc9, CardInc10;
     public GameObject CardBonus1, CardBonus2, CardBonus3, CardBonus4, CardBonus5, CardBonus6, CardBonus7, CardBonus8, CardBonus9, CardBonus10;
+    public GameObject CardChallenge1, CardChallenge2, CardChallenge3, CardChallenge4, CardChallenge5, CardChallenge6, CardChallenge7, CardChallenge8;
 
     private GameObject[] shuffledCards;
     private GameObject currentCard; // Store the currently displayed card
@@ -18,7 +20,8 @@ public class DrawCards : MonoBehaviour
         // Initialize the shuffled cards array and shuffle it initially
         shuffledCards = new GameObject[] { CardExp1, CardExp2, CardExp3, CardExp4, CardExp5, CardExp6, CardExp7, CardExp8, CardExp9, cardExp10, 
             CardInc1, CardInc2, CardInc3, CardInc4, CardInc5, CardInc6, CardInc7, CardInc8, CardInc9, CardInc10,
-            CardBonus1, CardBonus2, CardBonus3, CardBonus4, CardBonus5, CardBonus6, CardBonus7, CardBonus8, CardBonus9, CardBonus10
+            CardBonus1, CardBonus2, CardBonus3, CardBonus4, CardBonus5, CardBonus6, CardBonus7, CardBonus8, CardBonus9, CardBonus10,
+            CardChallenge1, CardChallenge2, CardChallenge3, CardChallenge4, CardChallenge5, CardChallenge6, CardChallenge7, CardChallenge8
         };
         ShuffleVariables();
     }
@@ -28,47 +31,56 @@ public class DrawCards : MonoBehaviour
         int n = shuffledCards.Length;
         for (int i = n - 1; i > 0; i--)
         {
-            // Generate a random index within the remaining unshuffled cards
             int randIndex = Random.Range(0, i + 1);
-
-            // Swap the current card with the randomly selected card
             GameObject temp = shuffledCards[i];
             shuffledCards[i] = shuffledCards[randIndex];
             shuffledCards[randIndex] = temp;
         }
 
-        currentIndex = 0; // Reset the current index to the beginning of the shuffled cards
-        sortingOrder = 0; // Reset the sorting order
+        currentIndex = 0;
+        sortingOrder = 0;
     }
 
     public void OnMouseDown()
     {
         if (currentIndex >= shuffledCards.Length)
         {
-            // If we have drawn all the cards, do nothing
             return;
         }
 
         if (currentCard != null)
         {
-            Destroy(currentCard); // Destroy the previous card
+            Destroy(currentCard);
         }
 
         Vector3 cardPosition = new Vector3(0, 0, currentIndex * cardOffset);
         currentCard = Instantiate(shuffledCards[currentIndex], cardPosition, Quaternion.identity);
         Debug.Log("Card instantiated: " + shuffledCards[currentIndex].name);
 
-        // Set the sorting order for the card to change its rendering order
-        SpriteRenderer spriteRenderer = currentCard.GetComponent<SpriteRenderer>();
+        // Access values from CardValues script
+        CardValues cardValues = currentCard.GetComponent<CardValues>();
+        if (cardValues != null)
+        {
+            // Apply the values to your progress bars
+            starChange += cardValues.starChange;
+            coins += cardValues.coinsChange;
+            energy += cardValues.energyChange;
+        }
+
+        // Set sorting order
+        SetSortingOrder(currentCard);
+
+        currentIndex++;
+    }
+    void SetSortingOrder(GameObject card)
+    {
+        SpriteRenderer spriteRenderer = card.GetComponent<SpriteRenderer>();
         if (spriteRenderer != null)
         {
             spriteRenderer.sortingOrder = sortingOrder;
             sortingOrder++;
         }
-
-        currentIndex++;
     }
-
     // Update is called once per frame
     void Update()
     {
