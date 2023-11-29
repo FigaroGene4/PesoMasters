b5893ba295e8c93ae7bb18e614370aa046f3ce44
FigaroGene4 <markgenesis37@gmail.com>
2023-11-29 20:36:22 +0800

    Buttons-InGame-SetActiveFalse Kapag di panaka draw
    Working na rin no Button

--- Assets/Scripts/countCards.cs
+++ Assets/Scripts/countCards.cs
@@ -4,35 +4,58 @@ using UnityEngine.UI;
 public class countCards : MonoBehaviour
 
 {
-    public Button yesButton;
+    public Button yesButton, noButton;
     public Text CardsLeft; // Reference to your UI Text element
     private int clickCount = 38; // Start with 40 clicks test
+    int clickCountz = 38;
 
     private void Start()
     {
         // Initialize the text to display the initial value (40)
+
         UpdateText();
-      
-        
+        if (clickCount != 0)
+        {
+            
+
+        }
+
+        if (clickCount < 38)
+        {
+            yesButton.gameObject.SetActive(true);
+            noButton.gameObject.SetActive(true);
+        }
     }
 
    
 
     public  void OnMouseDown()
     {
-      
-            clickCount = Mathf.Max(0, clickCount - 1);
+        clickCount = Mathf.Max(0, clickCount - 1);
 
-            // Update the text with the new value
-            UpdateText();
+        clickCountz--;
+        Debug.Log("Click" +clickCount);
 
-        if (clickCount == 0)
+        // Update the text with the new value
+        UpdateText();
+
+        if (clickCountz < 0)
         {
             // Display "Game Over" message in the Unity debug log
             Debug.Log("Game Over");
             yesButton.interactable = false;
-
+            noButton.interactable = false;
+            
         }
+
+        
+        else
+        {
+
+            yesButton.gameObject.SetActive(true);
+            noButton.gameObject.SetActive(true);
+        }
+    
     }
     
 
