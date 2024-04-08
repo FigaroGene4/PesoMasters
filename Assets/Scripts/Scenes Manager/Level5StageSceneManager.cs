25a4c05039505017de4a863b4446a6f4f21cd933
Jedcleofe <129458179+Jedcleofe@users.noreply.github.com>
2024-04-08 20:56:09 +0800

    Level 4 and 5 Stage

--- Assets/Scripts/Scenes Manager/Level5StageSceneManager.cs
+++ Assets/Scripts/Scenes Manager/Level5StageSceneManager.cs
@@ -9,18 +9,16 @@ public class Level5StageSceneManager : MonoBehaviour
     {
         SceneManager.LoadScene("GameMapLevel5");
     }
-    /*
-    public void Lvl4Stage1()
+    public void Lvl5Stage1()
     {
-        SceneManager.LoadScene("GamesceneLvl3");
+        SceneManager.LoadScene("GamesceneLvl5Stage1");
     }
-    public void Lvl4Stage2()
+    public void Lvl5Stage2()
     {
-        SceneManager.LoadScene("GamesceneLvl3");
+        SceneManager.LoadScene("GamesceneLvl5Stage2");
     }
-    public void Lvl4Stage3()
+    public void Lvl5Stage3()
     {
-        SceneManager.LoadScene("GamesceneLvl3");
+        SceneManager.LoadScene("GamesceneLvl5Stage3");
     }
-    */
 }
